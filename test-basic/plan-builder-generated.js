1378bfa132b90bf588ad095485c480639828c851
root <root@rh8v-node-1.dev.progress.com>
2025-06-26 14:32:15 -0700

    Auto-gen

--- test-basic/plan-builder-generated.js
+++ test-basic/plan-builder-generated.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 MarkLogic Corporation
+ * Copyright (c) 2003-2025 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,22 +23,11 @@ const marklogic = require('../');
 const p = marklogic.planBuilder;
 
 const pbb = require('./plan-builder-base');
-const testlib = require("../etc/test-lib");
 const testPlan = pbb.testPlan;
 const getResult = pbb.getResult;
-let serverConfiguration = {};
 
 describe('plan builder', function() {
-  describe('expression functions', function() {
-      before(function (done) {
-          this.timeout(6000);
-          try {
-              testlib.findServerConfiguration(serverConfiguration);
-              setTimeout(()=>{done();}, 3000);
-          } catch(error){
-              done(error);
-          }
-      });
+  describe('expression functions', function() { 
     it('cts.box#4', function(done) {
         testPlan([p.xs.double(1), p.xs.double(2), p.xs.double(3), p.xs.double(4)], p.cts.box(p.col("1"), p.col("2"), p.col("3"), p.col("4")))
           .then(function(response) { 
@@ -395,14 +384,14 @@ describe('plan builder', function() {
             should(String(getResult(response).value).replace(/^ /, '')).equal('10:09:08:00');
             done();
         }).catch(done);
-    });
+    }); 
     it('fn.head#1', function(done) {
         testPlan([[p.xs.string("a"), p.xs.string("b"), p.xs.string("c")]], p.fn.head(p.col("1")))
-          .then(function(response) {
-              should(String(getResult(response).value).replace(/^ /, '')).equal('a');
+          .then(function(response) { 
+            should(String(getResult(response).value).replace(/^ /, '')).equal('a');
             done();
         }).catch(done);
-    });
+    }); 
     it('fn.hoursFromDateTime#1', function(done) {
         testPlan([p.xs.dateTime("2016-01-02T10:09:08Z")], p.fn.hoursFromDateTime(p.col("1")))
           .then(function(response) { 
@@ -594,12 +583,8 @@ describe('plan builder', function() {
     }); 
     it('fn.prefixFromQName#1', function(done) {
         testPlan([p.xs.QName("abc")], p.fn.prefixFromQName(p.col("1")))
-          .then(function(response) {
-              if(serverConfiguration.serverVersion < 12){
-                  should(getResult(response).value).eql(null);
-              } else {
-                  should(String(getResult(response).value).replace(/^ /, '')).equal('');
-              }
+          .then(function(response) { 
+            should(String(getResult(response).value).replace(/^ /, '')).equal('');
             done();
         }).catch(done);
     }); 
@@ -948,7 +933,7 @@ describe('plan builder', function() {
     }); 
     it('geo.geohashNeighbors#1', function(done) {
         testPlan([p.xs.string("s01mtw")], p.geo.geohashNeighbors(p.col("1")))
-          .then(function(response) {
+          .then(function(response) { 
             should(getResult(response).value).eql({"NE":"s01mtz", "S":"s01mtt", "E":"s01mty", "W":"s01mtq", "SW":"s01mtm", "N":"s01mtx", "SE":"s01mtv", "NW":"s01mtr"});
             done();
         }).catch(done);
@@ -961,15 +946,12 @@ describe('plan builder', function() {
         }).catch(done);
     }); 
     it('geo.geohashSubhashes#1', function(done) {
-        if(serverConfiguration.serverVersion < 11){
-            this.skip();
-        }
         testPlan([p.xs.string("s01mtw")], p.geo.geohashSubhashes(p.col("1")))
           .then(function(response) { 
             should(getResult(response).value).eql(["s01mtw0", "s01mtw1", "s01mtw2", "s01mtw3", "s01mtw4", "s01mtw5", "s01mtw6", "s01mtw7", "s01mtw8", "s01mtw9", "s01mtwb", "s01mtwc", "s01mtwd", "s01mtwe", "s01mtwf", "s01mtwg", "s01mtwh", "s01mtwj", "s01mtwk", "s01mtwm", "s01mtwn", "s01mtwp", "s01mtwq", "s01mtwr", "s01mtws", "s01mtwt", "s01mtwu", "s01mtwv", "s01mtww", "s01mtwx", "s01mtwy", "s01mtwz"]);
             done();
-        }).catch(error => done(error));
-    });
+        }).catch(done);
+    }); 
     it('geo.geohashSubhashes#2', function(done) {
         testPlan([p.xs.string("s01mtw"), p.xs.string("S")], p.geo.geohashSubhashes(p.col("1"), p.col("2")))
           .then(function(response) { 
@@ -979,10 +961,8 @@ describe('plan builder', function() {
     }); 
     it('geo.parseWkt#1', function(done) {
         testPlan([p.xs.string("LINESTRING(-112.25 47.1,-112.3 47.1,-112.4 47.2)")], p.geo.parseWkt(p.col("1")))
-          .then(function(response) {
-              const responseValue = (serverConfiguration.serverVersion >= 11)?"LINESTRING(-112.25 47.100002,-112.3 47.100002,-112.39999 47.199997)":
-                  "LINESTRING(-112.25 47.1,-112.3 47.1,-112.4 47.2)";
-            should(getResult(response).value).eql(responseValue);
+          .then(function(response) { 
+            should(getResult(response).value).eql("LINESTRING(-112.25 47.100002,-112.3 47.100002,-112.39999 47.199997)");
             done();
         }).catch(done);
     }); 
@@ -1736,39 +1716,33 @@ describe('plan builder', function() {
         }).catch(done);
     }); 
     it('vec.base64Decode#1', function(done) {
-        if(serverConfiguration.serverVersion < 12) {
-            this.skip();
-        }
         testPlan([p.xs.string("abc")], p.vec.base64Decode(p.col("1")))
-          .then(function(response) {
-            should(String(getResult(response).value).replace(/^ /, '')).equal("abc");
+          .then(function(response) { 
+            should(String(getResult(response).value).replace(/^ /, '')).equal(null);
             done();
         }).catch(done);
-    });
+    }); 
     it('vec.vectorScore#2', function(done) {
-        if(serverConfiguration.serverVersion < 12) {
-            this.skip();
-        }
-        // Updating below test since server throws Error : VEC-VECTORSCORE: vec:vector-score(1, 2) --
-        // Invalid argument passed to vector scoring function: arg2 invalid: Similarity must be between -1.0 and 1.0
-        testPlan([p.xs.unsignedInt(1), p.xs.double(1.2)], p.vec.vectorScore(p.col("1"), 0.75))
-          .then(function(response) {
-            should(String(getResult(response).value).replace(/^ /, '')).equal('1');
+        testPlan([p.xs.unsignedInt(1), p.xs.double(1.2)], p.vec.vectorScore(p.col("1"), p.col("2")))
+          .then(function(response) { 
+            should(String(getResult(response).value).replace(/^ /, '')).equal('227273');
             done();
         }).catch(done);
-    });
+    }); 
     it('vec.vectorScore#3', function(done) {
-        if(serverConfiguration.serverVersion < 12) {
-            this.skip();
-        }
-        // Updating below test since server throws Error :arg2 invalid:Similarity must be between -1.0 and 1.0
-        //arg3 invalid: Similarity weight must be between 0.0 and 1.0
-        testPlan([p.xs.unsignedInt(1), p.xs.double(1.2), p.xs.double(1.2)], p.vec.vectorScore(p.col("1"), 0.75, 0.75))
-          .then(function(response) {
-            should(String(getResult(response).value).replace(/^ /, '')).equal('2');
+        testPlan([p.xs.unsignedInt(1), p.xs.double(1.2), p.xs.double(1.2)], p.vec.vectorScore(p.col("1"), p.col("2"), p.col("3")))
+          .then(function(response) { 
+            should(String(getResult(response).value).replace(/^ /, '')).equal(null);
             done();
         }).catch(done);
-    });
+    }); 
+    it('vec.vectorScore#4', function(done) {
+        testPlan([p.xs.unsignedInt(1), p.xs.double(1.2), p.xs.double(1.2), p.xs.double(1.2)], p.vec.vectorScore(p.col("1"), p.col("2"), p.col("3"), p.col("4")))
+          .then(function(response) { 
+            should(String(getResult(response).value).replace(/^ /, '')).equal(null);
+            done();
+        }).catch(done);
+    }); 
     it('xdmp.add64#2', function(done) {
         testPlan([p.xs.unsignedLong(123), p.xs.unsignedLong(456)], p.xdmp.add64(p.col("1"), p.col("2")))
           .then(function(response) { 
@@ -2169,9 +2143,6 @@ describe('plan builder', function() {
         }).catch(done);
     }); 
     it('xdmp.unquote#1', function(done) {
-        if(serverConfiguration.serverVersion < 11){
-            this.skip();
-        }
         testPlan([p.xs.string("[123]")], p.xdmp.unquote(p.col("1")))
           .then(function(response) { 
             should(getResult(response).value).eql([123]);
